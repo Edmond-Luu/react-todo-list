9f05d8a0acb3ac893bd516c43189be35ac026a5c
Edmond Luu <eluu472@gmail.com>
2022-08-22 17:02:33 -0700

    Created editing functionality

--- src/App.js
+++ src/App.js
@@ -11,9 +11,19 @@ function App() {
     title: '',
     desc: '',
     category: 'personal',
-    id: ''
+    id: '',
+    edit: false
   })
-  const [pinnedList, setPinnedList] = React.useState(JSON.parse(localStorage.getItem("donList")) || []);
+
+  const [editData, setEditData] = React.useState(JSON.parse(localStorage.getItem("editData")) || {
+    title: '',
+    desc: '',
+    category: '',
+    id: '',
+  })
+
+  const [pinnedList, setPinnedList] = React.useState(JSON.parse(localStorage.getItem("pinnedList")) || []);
+  const [editMode, setEditMode] = React.useState(JSON.parse(localStorage.getItem("editMode")) || false)
 
   React.useEffect(() => {
     localStorage.setItem("fullList", JSON.stringify(fullList))
@@ -26,6 +36,15 @@ function App() {
   React.useEffect(() => {
     localStorage.setItem("pinnedList", JSON.stringify(pinnedList))
   }, [pinnedList])
+
+  React.useEffect(() => {
+    localStorage.setItem("editData", JSON.stringify(editData))
+  }, [editData])
+
+  React.useEffect(() => {
+    localStorage.setItem("editMode", JSON.stringify(editMode))
+    console.log(editMode)
+  }, [editMode])
 
 
   function handleChange(event) {
@@ -45,7 +64,8 @@ function App() {
       title: '',
       desc: '',
       category: 'personal',
-      id: ''
+      id: '',
+      edit: false
     })
   }
 
@@ -63,6 +83,7 @@ function App() {
   }
 
   function deleteAll() {
+    setEditMode(false);
     setFullList([]);
   }
 
@@ -100,9 +121,9 @@ function App() {
       },
       ...oldList])
   }
-  
+
   function unpinAll() {
-    for (const item of pinnedList){
+    for (const item of pinnedList) {
       setFullList(oldList => [
         {
           title: item.title,
@@ -115,6 +136,91 @@ function App() {
     setPinnedList([]);
   }
 
+  function toggleEditOn(event, updateId) {
+    const tempList = fullList
+    const tempItem = tempList.find(item => item.id === updateId)
+    const updateIndex = tempList.findIndex(item => item.id === updateId);
+
+    setEditMode(!editMode);
+
+    tempList.splice(updateIndex, 1, {
+      title: tempItem.title,
+      desc: tempItem.desc,
+      category: tempItem.category,
+      id: tempItem.id,
+      edit: !tempItem.edit
+    });
+
+    setFullList([...tempList]);
+
+    setEditData({
+      title: tempItem.title,
+      desc: tempItem.desc,
+      category: tempItem.category,
+      id: tempItem.id
+    });
+  }
+
+  function toggleEditOff(event, updateId) {
+    const tempList = fullList
+    const tempItem = tempList.find(item => item.id === updateId)
+    const updateIndex = tempList.findIndex(item => item.id === updateId);
+
+    setEditMode(!editMode);
+
+    tempList.splice(updateIndex, 1, {
+      title: tempItem.title,
+      desc: tempItem.desc,
+      category: tempItem.category,
+      id: tempItem.id,
+      edit: !tempItem.edit
+    });
+
+    setFullList([...tempList]);
+
+    setEditData({
+      title: "",
+      desc: "",
+      category: "",
+      id: ""
+    });
+  }
+
+  function editChange(event) {
+    const { name, value } = event.target;
+    setEditData(oldData => {
+      return {
+        ...oldData,
+        [name]: value,
+        id: nanoid()
+      }
+    })
+  }
+
+  function editSubmit(event, updateId) {
+    const theList = fullList
+    const theItem = theList.find(item => item.id === updateId)
+    const updateIndex = theList.findIndex(item => item.id === updateId);
+
+    setEditMode(!editMode);
+
+    theList.splice(updateIndex, 1, {
+      title: editData.title,
+      desc: editData.desc,
+      category: editData.category,
+      id: editData.id,
+      edit: false
+    });
+
+    setFullList([...theList]);
+
+    setEditData({
+      title: "",
+      desc: "",
+      category: "",
+      id: ""
+    });
+  }
 
   const condStyle = {
     display: fullList.length ? '' : 'none'
@@ -131,8 +237,15 @@ function App() {
         category={item.category}
         key={item.id}
         id={item.id}
+        editMode={editMode}
+        edit={item.edit}
+        editData={editData}
         deleteItem={deleteItem}
         pinItem={pinItem}
+        toggleEditOn={toggleEditOn}
+        toggleEditOff={toggleEditOff}
+        editChange={editChange}
+        editSubmit={editSubmit}
       />
     )
   })
@@ -162,7 +275,7 @@ function App() {
           handleClear={handleClear}
         />
         <div className="allPinned">
-          <h3 style={pinnedStyle}>Pinned Items <Icon.PinFill/></h3>
+          <h3 style={pinnedStyle}>Pinned Items <Icon.PinFill /></h3>
           <button type="button" className="btn btn-warning unpinBtn" style={pinnedStyle} onClick={unpinAll}>Unpin All Items</button>
           <button type="button" className="btn btn-danger" style={pinnedStyle} onClick={deleteAllPinned}>Delete All Pinned Items</button>
           {pinnedItemElements}
@@ -170,7 +283,7 @@ function App() {
       </div>
       <div className="grid2">
         <p className="caption">Made completely from scratch with ❤️ by <a href="https://edmond-luu.github.io" target="_blank" rel="noreferrer">Edmond Luu</a></p>
-        <h3 style={condStyle}>Todo Items <Icon.ListCheck/></h3>
+        <h3 style={condStyle}>Todo Items <Icon.ListCheck /></h3>
         <button type="button" className="btn btn-danger" style={condStyle} onClick={deleteAll}>Delete All Items</button>
         {itemElements}
       </div>
--- src/Item.js
+++ src/Item.js
@@ -11,12 +11,40 @@ export default function Item(props) {
         <div className="card" style={itemStyle}>
             <div className="card-body">
                 <div>
-                    <h2 className="todoTitle">{props.title}</h2>
-                    <p className="todoDesc">{props.desc}</p>
+                    {props.edit ?
+                        <input value={props.editData.title} onChange={props.editChange} name="title" className="form-control editInput" /> :
+                        <h2 className="todoTitle">{props.title}</h2>}
+
+                    {props.edit ?
+                        <textarea value={props.editData.desc} onChange={props.editChange} name="desc" className="form-control editTextArea" /> :
+                        <p className="todoDesc">{props.desc}</p>}
+
+                    {props.edit ?
+                        <div className="spacer">
+                            <select value={props.editData.category} onChange={props.editChange} name="category" className="form-select editSelection">
+                                <option value="personal">Personal</option>
+                                <option value="work">Work</option>
+                            </select>
+                        </div> :
+                        ""}
                 </div>
                 <div>
-                    <button type="button" className="btn btn-primary delBtn littleBtn" onClick={(e) => props.pinItem(e, props.title, props.desc, props.category, props.id)}><Icon.Pin /></button>
-                    <button type="button" className="btn btn-danger delBtn" onClick={(e) => props.deleteItem(e, props.id)}><Icon.Trash /></button>
+
+                    {props.edit ?
+                        <button type="button" className="btn btn-success delBtn littleBtn" onClick={(e) => props.editSubmit(e, props.id)}><Icon.CheckLg /></button> :
+                        <button type="button" className="btn btn-primary delBtn littleBtn" onClick={(e) => props.pinItem(e, props.title, props.desc, props.category, props.id)}><Icon.Pin /></button>
+                    }
+
+                    {props.editMode ?
+                        "":
+                        <button type="button" className="btn btn-warning littleBtn" onClick={(e) => props.toggleEditOn(e, props.id)}><Icon.Pencil /></button>}
+
+
+                    {props.edit ?
+                        <button type="button" className="btn btn-secondary delBtn" onClick={(e) => props.toggleEditOff(e, props.id)}><Icon.XLg /></button> :
+                        <button type="button" className="btn btn-danger delBtn" onClick={(e) => props.deleteItem(e, props.id)}><Icon.Trash /></button>}
+
+
                 </div>
             </div>
         </div>
